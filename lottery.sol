5a3fe5b42739845b61400f43fbc293e0cfcaa02c
hamicodes <hamza.codes@outlook.com>
2023-02-27 01:59:38 +0500

    prohibit owner participation

--- lottery.sol
+++ lottery.sol
@@ -1,33 +1,46 @@
- 
 pragma solidity >=0.8.0 <0.8.18;
+
+/* 
+    In this version, the owner is banned from participating in the lottery.
+ */
 
 contract Lottery {
     address payable[] public players;
     address public owner;
 
-    constructor(){
+    constructor() {
         owner = msg.sender;
     }
 
     receive() external payable {
+        require(msg.sender != owner, "Owner cannot participate");
         require(msg.value == 0.1 ether, "Value must be 0.1 ether");
         players.push(payable(msg.sender));
     }
 
-    function getBalance() public view returns(uint) {
+    function getBalance() public view returns (uint) {
         require(msg.sender == owner);
         return address(this).balance;
     }
 
-    function getPlayers() public view returns(address payable[] memory) {
+    function getPlayers() public view returns (address payable[] memory) {
         require(msg.sender == owner);
         return players;
     }
 
     // Computed using deterministic values
     // Not to be used in production
-    function random() internal view returns(uint){
-        return uint(keccak256(abi.encodePacked(block.timestamp, block.difficulty, players.length)));
+    function random() internal view returns (uint) {
+        return
+            uint(
+                keccak256(
+                    abi.encodePacked(
+                        block.timestamp,
+                        block.difficulty,
+                        players.length
+                    )
+                )
+            );
     }
 
     function rewardWinner() public {
@@ -38,7 +51,7 @@ contract Lottery {
         address payable winner = players[i];
         winner.transfer(getBalance());
 
-       // Reset Lottery
-       players = new address payable[](0);
+        // Reset Lottery
+        players = new address payable[](0);
     }
 }
