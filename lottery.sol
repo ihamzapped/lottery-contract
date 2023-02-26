f0680b483d6468b56ece0a1bfc5b38fbf8211243
hamicodes <hamza.codes@outlook.com>
2023-02-27 02:04:16 +0500

    owner is participant auto

--- lottery.sol
+++ lottery.sol
@@ -1,12 +1,16 @@
- 
 pragma solidity >=0.8.0 <0.8.18;
+
+/* 
+    In this variant the owner is automatically added to the lottery without fee, upon deployment.
+ */
 
 contract Lottery {
     address payable[] public players;
     address public owner;
 
-    constructor(){
+    constructor() {
         owner = msg.sender;
+        players.push(payable(owner));
     }
 
     receive() external payable {
@@ -14,20 +18,29 @@ contract Lottery {
         players.push(payable(msg.sender));
     }
 
-    function getBalance() public view returns(uint) {
+    function getBalance() public view returns (uint) {
         require(msg.sender == owner);
         return address(this).balance;
     }
 
-    function getPlayers() public view returns(address payable[] memory) {
+    function getPlayers() public view returns (address payable[] memory) {
         require(msg.sender == owner);
         return players;
     }
 
     // Computed using deterministic values
     // Not to be used in production
-    function random() internal view returns(uint){
-        return uint(keccak256(abi.encodePacked(block.timestamp, block.difficulty, players.length)));
+    function random() internal view returns (uint) {
+        return
+            uint(
+                keccak256(
+                    abi.encodePacked(
+                        block.timestamp,
+                        block.difficulty,
+                        players.length
+                    )
+                )
+            );
     }
 
     function rewardWinner() public {
@@ -38,7 +51,7 @@ contract Lottery {
         address payable winner = players[i];
         winner.transfer(getBalance());
 
-       // Reset Lottery
-       players = new address payable[](0);
+        // Reset Lottery
+        players = new address payable[](0);
     }
 }
